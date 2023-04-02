62de6f5d7cef9dee49e4a9be83b344a74e7a5454
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-04-02 20:17:04 -0300

    minificando html

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
+function html() {
+  return gulp
+    .src('./src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist/'));
+}
 function styles() {
   return gulp
     .src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./'));
 }
 
 function scripts() {
@@ -24,8 +31,9 @@ function images() {
     .pipe(gulp.dest('./dist/img'));
 }
 
-exports.default = gulp.parallel(styles, scripts, images);
+exports.default = gulp.parallel(styles, scripts, images, html);
 exports.watch = function () {
   gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
   gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+  gulp.watch('./src/scripts/*.js', gulp.parallel(html));
 };
